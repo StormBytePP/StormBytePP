230407e59df04bdad61923a1de629eae0f3c2811
David C. Manuelda <StormByte@gmail.com>
2024-11-04 17:55:44 +0100

    Moved definitions

--- cmake/Definitions.cmake
+++
@@ -1,7 +0,0 @@
-file(CONFIGURE OUTPUT ${CMAKE_BINARY_DIR}/generated/StormByte.h CONTENT "#pragma once
-
-#define STORMBYTE_VERSION \"${${PROJECT_NAME}_VERSION}\"")
-
-file(CONFIGURE OUTPUT ${CMAKE_BINARY_DIR}/generated/StormByte/Features.h CONTENT "#pragma once
-
-#define STORMBYTE_SQLITE ${ENABLE_SQLITE}")
