dc71a9d72ca9e592c2f80ae71e9dcdf65e4b7e0b
David C. Manuelda <StormByte@gmail.com>
2024-11-06 13:29:23 +0100

    Dropped C char* handling and use strings directly

--- StormByte/system/exception.cxx
+++ StormByte/system/exception.cxx
@@ -4,50 +4,8 @@
 
 using namespace StormByte::System;
 
-Exception::Exception(const std::string& message):m_data(nullptr) {
-	copy(message);
-}
-
-Exception::Exception(const Exception& ex):m_data(nullptr) {
-	copy(ex.m_data);
-}
-
-Exception::~Exception() noexcept {
-	free();
-}
-
-Exception& Exception::operator=(const Exception& ex) {
-	if (this != &ex) {
-		copy(ex.m_data);
-	}
-	return *this;
-}
-
-Exception& Exception::operator=(Exception&& ex) noexcept {
-	if (this != &ex) {
-		free();
-		m_data = ex.m_data;
-		ex.m_data = nullptr;
-	}
-	return *this;
-}
+Exception::Exception(const std::string& message):m_data(message) {}
 
 const char* Exception::what() const noexcept {
-	return m_data;
-}
-
-void Exception::copy(const char* message) {
-	free();
-	std::memcpy(m_data, message, std::strlen(message));
-}
-
-void Exception::copy(const std::string& message) {
-	copy(message.c_str());
-}
-
-void Exception::free() {
-	if (m_data) {
-		std::free(m_data);
-		m_data = nullptr;
-	}
+	return m_data.c_str();
 }
--- StormByte/system/exception.hxx
+++ StormByte/system/exception.hxx
@@ -8,18 +8,14 @@ namespace StormByte::System {
 	class STORMBYTE_PUBLIC Exception {
 		public:
 			Exception(const std::string&);
-			Exception(const Exception&);
-			Exception(Exception&) noexcept;
-			Exception& operator=(const Exception&);
-			Exception& operator=(Exception&&) noexcept;
-			virtual ~Exception() noexcept;
+			Exception(const Exception&)					= default;
+			Exception(Exception&) noexcept				= default;
+			Exception& operator=(const Exception&)		= default;
+			Exception& operator=(Exception&&) noexcept	= default;
+			virtual ~Exception() noexcept				= default;
 			virtual const char* what() const noexcept;
 
 		private:
-			char* m_data;
-
-			void copy(const char*);
-			void copy(const std::string&);
-			void free();
+			std::string m_data;
 	};
 }
